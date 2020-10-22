40ba0f88e81e017cbb721139a78985755d1e8edd
Dwight Alejandro <dwight_alfaro_14@hotmail.com>
2020-10-22 16:05:20 -0500

    changes agregar tratamientos, ver tratamientos

--- app.js
+++ app.js
@@ -30,4 +30,52 @@ window.addEventListener('load', function(){
       ]
     });
 
+
+
+
+
 })
+
+
+
+
+
+/*register new tratment */
+try{
+  function viewTreatment () {
+    let typeTreatment = document.querySelector('#treatment').value;
+    switch (typeTreatment) {
+      case 1:
+        precio = 2500.0;
+        cuota = (precio/5);
+        break;
+      case 2:
+        precio = 1500.0;
+        cuota = (precio/5);
+        break;
+      case 3:
+        precio = 3500.0;
+        cuota = (precio/5);
+        break;   
+      case 4:
+        precio = 3000.0;
+        cuota = (precio/5);
+        break; 
+      default:
+        break;
+    }
+    // cuota = precio / 5;
+  
+    document.querySelector('#precio').value = `El precio es ${ precio }`;
+    document.querySelector('#cuota').value = `La cuota mensual es de ${ cuota }`;
+  
+    console.log(typeTreatment);
+    console.log(precio);
+    console.log(cuota);
+  
+  }
+  }
+  catch (error){
+    console.log(error);
+  }
+
