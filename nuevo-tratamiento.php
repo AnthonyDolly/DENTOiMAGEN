e9f95257aa1b232afaabc389aec38401243fe762
Jacob <AnthonyLuiggyDollyO@gmail.com>
2020-11-28 00:30:24 -0500

    registrar tratamiento

--- nuevo-tratamiento.php
+++ nuevo-tratamiento.php
@@ -107,19 +107,19 @@
 
         <main>
 
-            <div class="content-register-treatment">
+        <div class="content-register-treatment">
                 <h1>Registrar tratamiento</h1>
                 <div class="table-register-treatment">
-                    <form action="" method="POST">
+                    <form action="registrar-tratamiento.php" method="POST">
                         <div class="col-md-6 mb-3">
                             <label for="validationCustom01">DNI Paciente</label>
-                            <input type="text" class="form-control" id="dnipaciente" required>
+                            <input type="text" class="form-control" id="dnipaciente" name="DNIPaciente" required>
                         </div>
                         
-                        <div class="col-md-6 mb-3">
+                        <!-- <div class="col-md-6 mb-3">
                             <label class="" for="exampleFormControlInput1">Fecha</label>
                             <input type="date" class="form-control" id="exampleFormControlInput1" required>
-                        </div>
+                        </div> -->
                         <div class="col-md-6 mb-3">
                             <label for="treatment">Tratamiento</label>
                             <select id="treatment" name="treatment" class="form-control" onchange="viewTreatment()">
@@ -142,7 +142,7 @@
                         </div> -->
                         <div class="col-md-6 mb-3 ">
                             <label class="" for="exampleFormControlTextarea1">Descripción</label>
-                            <textarea class="form-control" id="exampleFormControlTextarea1" ></textarea>
+                            <textarea class="form-control" id="exampleFormControlTextarea1" name="descripcion"></textarea>
                         </div>
                         <!-- <div class="col-md-6 mb-3">
                             <label for="precio">Precio</label>
@@ -155,23 +155,21 @@
                         <div class="d-flex col-md-6 mb-3 justify-content-around">
                             <div class="form-check ">
                                 <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1"
-                                    value="option1" checked>
+                                    value="true" checked>
                                 <label class="form-check-label" for="gridRadios1">
                                     Activo
                                 </label>
                             </div>
                             <div class="form-check">
                                 <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2"
-                                    value="option2">
+                                    value="false">
                                 <label class="form-check-label" for="gridRadios2">
                                     Inactivo
                                 </label>
                             </div>
                         </div>
                         <div class="col-md-6 mb-3 btn-save-treatment">
-                            <a href="mis-tratamientos.php">
-                                <button type="submit" onclick="enlace()"  class="btn btn-primary">Guardar</button>
-                            </a>
+                            <button type="submit" class="btn btn-primary" name="btnGuardar">Guardar</button>
                         </div>
                     </form>
                 </div>
