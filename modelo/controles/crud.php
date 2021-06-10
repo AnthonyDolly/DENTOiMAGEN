68f544a9ca489286506bf6da329b97c258efa9bf
ELPA QUITO <jbrayams@hotmail.com>
2021-06-09 21:23:04 -0500

    procedimientos almacenados (terminado)

--- modelo/controles/crud.php
+++ modelo/controles/crud.php
@@ -7,18 +7,7 @@ class DatosControles extends Conexion {
     #----------------------------
     public function vistaControlModelo($datosModelo, $tabla) {
 
-        $st = Conexion::conectar()->prepare("SELECT DATE_FORMAT(cm.fecha,'%d/%m/%Y') AS Fecha, DATE_FORMAT(cm.fecha,'%H:%i') as Hora, concat(m.nombres, ' ', m.apellidos) AS Doctor, s.nombre AS Sede, cm.precioSesion 'Precio de control', cm.estadoPago AS 'Estado de pago', cm.asistencia AS Asistencia
-        FROM controles_mensuales cm
-        INNER JOIN clientes_tratamientos ct
-        ON cm.cliente_tratamiento_id = ct.id
-        INNER JOIN tratamientos t
-        ON ct.tratamiento_id = t.id
-        INNER JOIN medicos m
-        ON t.medico_id = m.id
-        INNER JOIN sedes s
-        ON t.sede_id = s.id
-        WHERE ct.cliente_id = $datosModelo
-        ;");
+        $st = Conexion::conectar()->prepare("CALL vistaControl($datosModelo)");
 
         $st->execute();
 
@@ -29,20 +18,7 @@ class DatosControles extends Conexion {
     #---------------------------------------
     public function vistaControlMedicoModelo($datosModelo, $tabla) {
 
-        $st = Conexion::conectar()->prepare("SELECT DATE_FORMAT(cm.fecha,'%d/%m/%Y') AS Fecha, DATE_FORMAT(cm.fecha,'%H:%i') as Hora, concat(c.nombres, ' ', c.apellidos) AS Paciente, s.nombre AS Sede, cm.precioSesion 'Precio de control', cm.estadoPago AS 'Estado de pago', cm.asistencia AS Asistencia, cm.id as 'ID CM', m.id as Medico
-        FROM controles_mensuales cm
-        INNER JOIN clientes_tratamientos ct
-        ON cm.cliente_tratamiento_id = ct.id
-        INNER JOIN tratamientos t
-        ON ct.tratamiento_id = t.id
-        INNER JOIN medicos m
-        ON t.medico_id = m.id
-        INNER JOIN clientes c
-        ON ct.cliente_id = c.id
-        INNER JOIN sedes s
-        ON t.sede_id = s.id
-        WHERE m.id = $datosModelo
-        ;");
+        $st = Conexion::conectar()->prepare("CALL vistaControlMedico($datosModelo)");
 
         $st->execute();
 
@@ -53,7 +29,7 @@ class DatosControles extends Conexion {
     #--------------------------------------------------
     public function registroControlMedicoModelo($datosModelo, $tabla) {
 
-        $st = Conexion::conectar()->prepare("INSERT INTO $tabla (fecha,precioSesion,estadoPago,cliente_tratamiento_id,asistencia) VALUES (:fecha,:precioSesion,:estadoPago,:CTID,:asistencia)");
+        $st = Conexion::conectar()->prepare("call registroControlMedico(:fecha,:precioSesion,:estadoPago,:CTID,:asistencia)");
 
         $st->bindParam(":fecha", $datosModelo["fecha"], PDO::PARAM_STR);
         $st->bindParam(":precioSesion", $datosModelo["precio"], PDO::PARAM_STR);
@@ -72,6 +48,7 @@ class DatosControles extends Conexion {
         $st->close();
     }
 
+   /* 
     #Eliminar control mensual por parte del medico
     #------------------------------------------------
     public function eliminarControlMedicoModelo($datosModelo, $tabla) {
@@ -87,27 +64,14 @@ class DatosControles extends Conexion {
         $st->close();
     }
 
-
+    */
 
     
     #Editar control mensual por parte del medico
     #------------------------------------------------
     public function editarCitaControlMensualModelo($datosModelo){
         
-        $st = Conexion::conectar()->prepare("SELECT cm.id as 'ID CM', cm.fecha as 'Fecha y Hora', concat(c.nombres, ' ', c.apellidos) AS Paciente, s.nombre AS Sede, cm.precioSesion 'Precio de control', cm.estadoPago AS 'Estado de pago', cm.asistencia AS Asistencia
-        FROM controles_mensuales cm
-        INNER JOIN clientes_tratamientos ct
-        ON cm.cliente_tratamiento_id = ct.id
-        INNER JOIN tratamientos t
-        ON ct.tratamiento_id = t.id
-        INNER JOIN medicos m
-        ON t.medico_id = m.id
-        INNER JOIN clientes c
-        ON ct.cliente_id = c.id
-        INNER JOIN sedes s
-        ON t.sede_id = s.id
-        WHERE cm.id = $datosModelo
-        ;");
+        $st = Conexion::conectar()->prepare("CALL editarCitaControlMensual($datosModelo)");
 
         $st->execute();
 
@@ -119,7 +83,7 @@ class DatosControles extends Conexion {
     #------------------------------------------------
     public function actualizarCitaControlMensualModelo($datosModelo, $tabla) {
 
-        $st = Conexion::conectar()->prepare("UPDATE $tabla set fecha=:fecha WHERE id=:idCM");
+        $st = Conexion::conectar()->prepare("call actualizarCitaControlMensual(:idCM,:fecha);");
 
         $st->bindParam(":idCM", $datosModelo["idCM"], PDO::PARAM_STR);
         $st->bindParam(":fecha", $datosModelo["fecha"], PDO::PARAM_STR);
@@ -139,7 +103,7 @@ class DatosControles extends Conexion {
     #------------------------------------------------
     public function eliminarCitaControlMensualModelo($datosModelo, $tabla) {
 
-        $st = Conexion::conectar()->prepare("DELETE FROM $tabla WHERE id=$datosModelo;");
+        $st = Conexion::conectar()->prepare("call eliminarCitaControlMensual($datosModelo);");
         
         if($st->execute()){
             return "success";
