a2343cb7c750be55f5055d3cc739c2e58ef602cc
ELPA QUITO <jbrayams@hotmail.com>
2021-06-30 02:27:30 -0500

    procedimientos

--- modelo/informacion_controles/crud.php
+++ modelo/informacion_controles/crud.php
@@ -34,20 +34,7 @@ class InformacionControles extends Conexion
     #----------------------------
     public function vistaInformacionControlesModelo($datosModelo, $tabla) {
 
-        $st = Conexion::conectar()->prepare("SELECT cm.fecha as Fecha, concat(m.nombres,' ',m.apellidos) AS 'Medico', m.id AS 'DNIMedico', ic.sede AS 'Sede', cm.precioSesion AS 'Importe', cm.estadoPago AS 'Estado de pago', cm.asistencia AS 'Asistencia', concat(c.nombres,' ',c.apellidos) AS 'Cliente', c.id AS 'DNICliente', ic.descripcion AS 'Descripcion', ic.id AS IdIC
-        FROM controles_mensuales cm
-        INNER JOIN informacion_controles ic
-        ON cm.id = ic.control_mensual_id
-        INNER JOIN clientes_tratamientos ct
-        ON cm.cliente_tratamiento_id = ct.id
-        INNER JOIN clientes c
-        ON ct.cliente_id = c.id
-        INNER JOIN tratamientos t
-        ON ct.tratamiento_id = t.id
-        INNER JOIN medicos m
-        ON t.medico_id = m.id
-        WHERE ic.cliente_id = $datosModelo
-        GROUP BY cm.id;");
+        $st = Conexion::conectar()->prepare("call vistaInformacionControles($datosModelo);");
 
         $st->execute();
 
@@ -58,20 +45,7 @@ class InformacionControles extends Conexion
     #----------------------------
     public function vistaPDFInformacionControlesModelo($datosModelo, $tabla) {
 
-        $st = Conexion::conectar()->prepare("SELECT DATE_FORMAT(cm.fecha, '%d/%m/%Y') as Fecha, concat(m.nombres,' ',m.apellidos) AS 'Medico', m.id AS 'DNIMedico', ic.sede AS 'Sede', cm.precioSesion AS 'Importe', cm.estadoPago AS 'Estado de pago', cm.asistencia AS 'Asistencia', concat(c.nombres,' ',c.apellidos) AS 'Cliente', c.id AS 'DNICliente', ic.descripcion AS 'Descripcion', ic.id AS IdIC
-        FROM controles_mensuales cm
-        INNER JOIN informacion_controles ic
-        ON cm.id = ic.control_mensual_id
-        INNER JOIN clientes_tratamientos ct
-        ON cm.cliente_tratamiento_id = ct.id
-        INNER JOIN clientes c
-        ON ct.cliente_id = c.id
-        INNER JOIN tratamientos t
-        ON ct.tratamiento_id = t.id
-        INNER JOIN medicos m
-        ON t.medico_id = m.id
-        WHERE ic.id = $datosModelo
-        GROUP BY cm.id;");
+        $st = Conexion::conectar()->prepare("call vistaPDFInformacionControles($datosModelo);");
 
         $st->execute();
 
--- modelo/informacion_tratamientos/crud.php
+++ modelo/informacion_tratamientos/crud.php
@@ -8,15 +8,7 @@ class InformacionTramientos extends Conexion
     #----------------------------
     public function vistaInformacionTramientosModelo($datosModelo, $tabla) {
 
-        $st = Conexion::conectar()->prepare("SELECT concat(m.nombres,' ',m.apellidos) AS 'Medico', ct.cliente_id AS 'DNICliente', ct.fecha_inicio as 'Fecha Inicio', tt.nombre as 'NombreTratamiento', ct.id  as 'idCT'
-        FROM clientes_tratamientos ct
-        INNER JOIN tratamientos t
-        ON ct.tratamiento_id = t.id
-        INNER JOIN tipos_tratamientos tt
-        on t.tipo_tratamiento_id = tt.id
-        INNER JOIN medicos m
-        ON t.medico_id = m.id
-        WHERE ct.cliente_id = $datosModelo;");
+        $st = Conexion::conectar()->prepare("call vistaInformacionTramientos($datosModelo);");
         
         $st->execute();
 
@@ -26,22 +18,7 @@ class InformacionTramientos extends Conexion
 
     public function vistaPDFInformacionTratamientosModelo($datosModelo, $tabla) {
 
-        $st = Conexion::conectar()->prepare("SELECT DATE_FORMAT(cm.fecha, '%d/%m/%Y') as Fecha, concat(m.nombres,' ',m.apellidos) AS 'Medico', m.id AS 'DNIMedico', ic.sede AS 'Sede', cm.precioSesion AS 'Importe', cm.estadoPago AS 'Estado de pago', cm.asistencia AS 'Asistencia', concat(c.nombres,' ',c.apellidos) AS 'Cliente', c.id AS 'DNICliente', ic.descripcion AS 'Descripcion', ct.fecha_inicio as 'Fecha Inicio', tt.nombre as 'NombreTratamiento'
-        FROM controles_mensuales cm
-        INNER JOIN informacion_controles ic
-        ON cm.id = ic.control_mensual_id
-        INNER JOIN clientes_tratamientos ct
-        ON cm.cliente_tratamiento_id = ct.id
-        INNER JOIN clientes c
-        ON ic.cliente_id = c.id
-        INNER JOIN tratamientos t
-        ON ct.tratamiento_id = t.id
-        INNER JOIN tipos_tratamientos tt
-        on t.tipo_tratamiento_id = tt.id
-        INNER JOIN medicos m
-        ON t.medico_id = m.id
-        WHERE ct.id = $datosModelo
-        GROUP BY cm.id;");
+        $st = Conexion::conectar()->prepare("call vistaPDFInformacionTratamientos($datosModelo);");
 
         // $st->bindParam(":dni", $datosModelo["dni"],PDO::PARAM_STR);
         // $st->bindParam(":nt", $datosModelo["nt"], PDO::PARAM_STR);
@@ -52,20 +29,7 @@ class InformacionTramientos extends Conexion
 
     public function vistaTituloPDFInformacionTratamientosModelo($datosModelo, $tabla) {
 
-        $st = Conexion::conectar()->prepare("SELECT concat(m.nombres,' ',m.apellidos) AS 'Medico', ct.cliente_id AS 'DNICliente', ct.fecha_inicio as 'Fecha Inicio', tt.nombre as 'NombreTratamiento', s.nombre as 'Sede', concat(c.nombres,' ',c.apellidos) AS 'Paciente', ct.descripcion as 'Descripcion', m.id as 'DNIMedico'
-        FROM clientes_tratamientos ct
-        INNER JOIN tratamientos t
-        ON ct.tratamiento_id = t.id
-        INNER JOIN clientes c
-        ON ct.cliente_id = c.id
-        INNER JOIN sedes s
-        ON t.sede_id = s.id
-        INNER JOIN tipos_tratamientos tt
-        on t.tipo_tratamiento_id = tt.id
-        INNER JOIN medicos m
-        ON t.medico_id = m.id
-        WHERE ct.cliente_id = :dni
-        and ct.id = :idCT;");
+        $st = Conexion::conectar()->prepare("call vistaTituloPDFInformacionTratamientos(:dni,:idCT);");
         $st->bindParam(":dni", $datosModelo["dni"],PDO::PARAM_STR);
         $st->bindParam(":idCT", $datosModelo["idCT"], PDO::PARAM_STR);
         $st->execute();
